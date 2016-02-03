f9cb15a0404cb813b4c4e0f949d628fa1162392d
Aurelia Catrice <catricea9@gmail.com>
2016-02-03 15:06:03 +0100

    debut serveur tcp

--- webserver/main.c
+++ webserver/main.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include "socket.h"
+
 int main(int argc, char **argv) {
+	int socket_client;
+	int socket_serveur= creer_serveur(8080);
+
 	/* Arnold Robbins in the LJ of February ’95, describing RCS */
-	if (argc > 1 && strcmp(argv[1], "-advice") == 0) {
+	if(argc > 1 && strcmp(argv[1], "-advice") == 0) {
 		printf("Don’t Panic!\n");
 		return 42;
 	}
 	printf("Need an advice?\n");
+
+
+
+	socket_client = accept(socket_serveur, NULL, NULL);
+	if(socket_client == -1) {
+		perror("accept");
+	/* traitement d ’ erreur */
+	}
+	/* On peut maintenant dialoguer avec le client */
+	const char * message_bienvenue = "Bonjour, bienvenue sur mon serveur\n ";
+	write(socket_client, message_bienvenue, strlen(message_bienvenue));
+
 	return 0;
 }
---
+++ webserver/socket.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int socket_serveur;
+ /* écoute sur toutes les interfaces */
+
+int creer_serveur(int port){
+
+	socket_serveur = socket (AF_INET, SOCK_STREAM, 0);
+
+	struct sockaddr_in saddr;
+	saddr.sin_family = AF_INET;				/* Socket ipv4 */
+	saddr.sin_port = htons (port); 			 /* Port d ’ écoute */
+	saddr.sin_addr.s_addr = INADDR_ANY ;	 /* écoute sur toutes les interfaces */
+
+	if (socket_serveur == -1) {
+		perror("socket_serveur");
+		return -1;
+	}
+
+	if(bind(socket_serveur, (struct sockaddr*)& saddr, sizeof(saddr)) == -1){
+		perror("bind socker_serveur");
+		return -1;
+	}
+
+	if(listen(socket_serveur, 10) == -1){
+		perror("listen socket_serveur");
+		return -1;
+	}
+	return 0;
+}
