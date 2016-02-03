5abb83593f0c44b64d08febdb3a6d768200d0041
Aurelia Catrice <catricea9@gmail.com>
2016-02-03 16:32:21 +0100

    correction reuseaddr

--- webserver/socket.c
+++ webserver/socket.c
@@ -15,11 +15,17 @@ int creer_serveur(int port){
 	saddr.sin_port = htons (port); 			 /* Port d ’ écoute */
 	saddr.sin_addr.s_addr = INADDR_ANY ;	 /* écoute sur toutes les interfaces */
 
-	if (socket_serveur == -1) {
+	if (socket_serveur == -1){
 		perror("socket_serveur");
 		return -1;
 	}
 
+	int optval = 1;
+	if(setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1){
+		perror("Can not set SO_REUSEADDR option");
+		return -1;
+	}
+	
 	if(bind(socket_serveur, (struct sockaddr*)& saddr, sizeof(saddr)) == -1){
 		perror("bind socket_serveur");
 		return -1;
@@ -29,5 +35,7 @@ int creer_serveur(int port){
 		perror("listen socket_serveur");
 		return -1;
 	}
+
+
 	return socket_serveur;
 }
